2a3791479a1defeb6b5ba2d354de7b0497987451
Andreas N <git@pixelde.su>
2018-09-19 01:54:43 +0200

    add Command model

---
+++ src/database/migrations/20180918235053-create-command.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Commands', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      enabled: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Commands');
+  }
+};
---
+++ src/database/models/command.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Command = sequelize.define('Command', {
+    name: DataTypes.STRING,
+    enabled: DataTypes.BOOLEAN
+  }, {});
+  Command.associate = function(models) {
+    // associations can be defined here
+  };
+  return Command;
+};
