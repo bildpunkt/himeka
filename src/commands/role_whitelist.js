80e2418a68ca6926d68e0b8ff3960d2a0b4afcff
Andreas N <git@pixelde.su>
2018-09-24 00:29:01 +0200

    add role whitelist command

---
+++ src/commands/role_whitelist.js
@@ -0,0 +1,52 @@
+const MessageCommand = require('../lib/commands/messageCommand')
+
+const DatabaseManager = require('../lib/databaseManager')
+const database = new DatabaseManager()
+const Role = database.models.Role
+
+module.exports = class EnableCommand extends MessageCommand {
+  constructor (args, config) {
+    super(args, config)
+
+    this.requireCommandPrefix = true
+    this.requireAdmin = true
+    this.commandName = 'whitelist-role'
+  }
+
+  static name () {
+    return 'whitelist-role'
+  }
+
+  command () {
+    const messageArguments = this.message.content
+      .slice(this.config.get('prefix').length)
+      .split(/ +/)
+
+    messageArguments.shift()
+    const roleName = messageArguments.join(' ')
+
+    Role.findOne({ where: { name: roleName } })
+      .then(role => {
+        if (role === null) {
+          this.message.channel.send('Role does not exist!')
+          throw new Error()
+        }
+
+        if (role.whitelisted === true) {
+          this.message.channel.send('Role is already whitelisted!')
+          throw new Error()
+        }
+
+        role
+          .updateAttributes({
+            whitelisted: true
+          })
+          .then(() => {
+            this.message.channel.send(
+              `Role \`${roleName}\` is now whitelisted!`
+            )
+          })
+      })
+      .catch(Error, () => {})
+  }
+}
