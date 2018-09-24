e0b6fb77d5dbe5e501fefafe12aaf57487495968
Andreas N <git@pixelde.su>
2018-09-24 03:13:43 +0200

    add JSDoc for AbstractCommand

--- src/lib/commands/abstractCommand.js
+++ src/lib/commands/abstractCommand.js
@@ -1,22 +1,54 @@
+/**
+ * AbstractCommand
+ *
+ * Base class for all commands
+ */
 module.exports = class AbstractCommand {
+  /**
+   * Constructor
+   *
+   * @param {array} args - array from arguments from discord.js event
+   * @param {ConfigManager} config - ConfigManager instance
+   */
   constructor (args, config) {
     this.config = config
 
     this.arguments = args
   }
 
+  /**
+   * Name of the command
+   *
+   * This is static because we have no properties in ES6 classes
+   */
   static name () {
     return 'abstract-command'
   }
 
+  /**
+   * Event type of the command
+   * https://discord.js.org/#/docs/main/stable/class/Client
+   *
+   * @throws Error, if not implemented
+   */
   static event () {
     throw Error('TODO: Not implemented')
   }
 
+  /**
+   * Wrapping function for command()
+   *
+   * Put code here that handles superficial stuff users don't
+   * need to mess with
+   */
   execute () {
     this.command()
   }
 
+  /**
+   * Code relevant to commands, which needs to be written in
+   * extended classes from this one
+   */
   command () {
     throw new Error('TODO: Not implemented')
   }
