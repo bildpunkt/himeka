1d5cd3b12199901e71ed6b5718230bec38cd6986
Andreas N <git@pixelde.su>
2018-09-24 03:30:23 +0200

    add JSDoc for RoleUpdateCommand

--- src/lib/commands/roleUpdateCommand.js
+++ src/lib/commands/roleUpdateCommand.js
@@ -1,6 +1,20 @@
 const AbstractCommand = require('./abstractCommand')
 
+/**
+ * RoleUpdateCommand
+ *
+ * Base class for Discord.Clients 'roleUpdate' event
+ */
 module.exports = class RoleUpdateCommand extends AbstractCommand {
+  /**
+   * Constructor
+   *
+   * @param {array} args - array from arguments from discord.js event
+   * @param {ConfigManager} config - ConfigManager instance
+   *
+   * @inner {Role} oldRole - role before update
+   * @inner {Role} newRole - role after update
+   */
   constructor (args, config) {
     super(args, config)
 
@@ -8,11 +22,17 @@ module.exports = class RoleUpdateCommand extends AbstractCommand {
     this.newRole = args[1]
   }
 
-  static event () {
-    return 'roleUpdate'
-  }
-
+  /**
+   * Name of the command
+   */
   static name () {
     return 'role-update-event'
+  }
+
+  /**
+   * Event type of the command
+   */
+  static event () {
+    return 'roleUpdate'
   }
 }
