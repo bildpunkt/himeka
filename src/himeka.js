b3bf98ab27df0cb4db372da7bf31ed40bffa39d0
Andreas N <git@pixelde.su>
2018-09-24 22:25:55 +0200

    add JSDoc for main class

--- src/himeka.js
+++ src/himeka.js
@@ -2,7 +2,21 @@ const Discord = require('discord.js')
 const CommandManager = require('./lib/commandManager')
 const DatabaseManager = require('./lib/databaseManager')
 
+/**
+ * Himeka
+ *
+ * Main class for the Discord bot
+ */
 module.exports = class Himeka {
+  /**
+   * Constructor
+   *
+   * @param {ConfigManager} config - instance of ConfigManager
+   *
+   * @inner {Discord.Client} client - instance of Discord.Client
+   * @inner {DatabaseManager} database - instance of DatabaseManager
+   * @inner {CommandManager} config - instance of ConfigManager
+   */
   constructor (config) {
     this.client = new Discord.Client()
 
@@ -12,6 +26,9 @@ module.exports = class Himeka {
     this.commands = new CommandManager(config, this.client, this.database)
   }
 
+  /**
+   * Function to start the bot!
+   */
   run () {
     this.client.login(this.config.get('token'))
   }
