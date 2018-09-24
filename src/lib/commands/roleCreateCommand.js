edf8bcaa8cd639077013071bf2e3ddb952b2de08
Andreas N <git@pixelde.su>
2018-09-24 03:24:05 +0200

    add JSDoc for RoleCreateCommand

--- src/lib/commands/roleCreateCommand.js
+++ src/lib/commands/roleCreateCommand.js
@@ -1,17 +1,36 @@
 const AbstractCommand = require('./abstractCommand')
 
+/**
+ * RoleCreateCommand
+ *
+ * Base class for Discord.Clients 'roleCreate' event
+ */
 module.exports = class RoleCreateCommand extends AbstractCommand {
+  /**
+   * Constructor
+   *
+   * @param {array} args - array from arguments from discord.js event
+   * @param {ConfigManager} config - ConfigManager instance
+   *
+   * @inner {Role} role - created role
+   */
   constructor (args, config) {
     super(args, config)
 
     this.role = args[0]
   }
 
-  static event () {
-    return 'roleCreate'
-  }
-
+  /**
+   * Name of the command
+   */
   static name () {
     return 'role-create-event'
+  }
+
+  /**
+   * Event type of the command
+   */
+  static event () {
+    return 'roleCreate'
   }
 }
