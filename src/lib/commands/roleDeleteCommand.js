c47c1b1f61ae0b5d13ef7ea8c62881862e4bd088
Andreas N <git@pixelde.su>
2018-09-24 03:27:13 +0200

    add JSDoc for RoleDeleteCommand

--- src/lib/commands/roleDeleteCommand.js
+++ src/lib/commands/roleDeleteCommand.js
@@ -1,17 +1,36 @@
 const AbstractCommand = require('./abstractCommand')
 
+/**
+ * RoleDeleteCommand
+ *
+ * Base class for Discord.Clients 'roleDelete' event
+ */
 module.exports = class RoleDeleteCommand extends AbstractCommand {
+  /**
+   * Constructor
+   *
+   * @param {array} args - array from arguments from discord.js event
+   * @param {ConfigManager} config - ConfigManager instance
+   *
+   * @inner {Role} role - deleted role
+   */
   constructor (args, config) {
     super(args, config)
 
     this.role = args[0]
   }
 
-  static event () {
-    return 'roleDelete'
-  }
-
+  /**
+   * Name of the command
+   */
   static name () {
     return 'role-delete-event'
+  }
+
+  /**
+   * Event type of the command
+   */
+  static event () {
+    return 'roleDelete'
   }
 }
